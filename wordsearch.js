91cc6e3a9732df3bcf7934977e03caf7cd2abbd5
raphpaquet <raphaelle.paquet@hotmail.com>
2020-11-05 00:00:39 +0000

    refacted function

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,44 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (letters) {
 
-module.exports = wordSearch
+  let newArr = [];
+  for (let k = 0; k < letters[0].length; k++) {
+    newArr.push([]);
+  }
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < letters[i].length; j++) {
+      newArr[j].push(letters[i][j]);
+    }
+  }
+  return newArr;
+};
+
+
+const wordSearch = (letters, word) => {
+  if (letters.length === 0) {
+    return false;
+  }
+
+  const horizontalJoin = letters.map(ls => ls.join('')); 
+  for (let l of horizontalJoin) { 
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  const verticalJoin = transpose(letters).map(ls => ls.join('')); 
+  for (let l of verticalJoin) { 
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+
+
+module.exports = wordSearch;
+
+
+
+
+
